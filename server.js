afe3dae7b9fcc8bbace5eb0ebdbaad6ab80a1f04
lotemeldar <lotemeldar@lotemeldar.com>
2025-06-30 15:48:05 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,16 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+// טוענים את קובץ ה־JSON עם ההצעות חוק
+const bills = require('./data/bills.json');
+
 app.get('/', (req, res) => {
   res.send('Welcome to the Kenescope API!');
+});
+
+// נתיב שמחזיר את רשימת ההצעות חוק
+app.get('/api/bills', (req, res) => {
+  res.json(bills);
 });
 
 app.listen(PORT, () => {
